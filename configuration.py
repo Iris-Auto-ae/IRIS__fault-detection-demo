496ee18a8033d8bfaa054fc577e517756425aa1f
foxelas <foxelas@outlook.com>
2023-12-11 11:02:19 +0200

    Update add ocr and measurement

--- configuration.py
+++ configuration.py
@@ -1,16 +1,20 @@
+from os.path import join as pathjoin
+
 demo_title = "Iris Auto AI Vision Demo"
+
+demo_logo = pathjoin("assets", "logo.png")
 
 demo_explanation = """
             ## Iris Auto AI Vision Technology
             #### Customize an AI assistant model
-            Give some example annotated images to the model and *train* it to run for your specific problem.
+            Give some example annotated images to the model and train it to run for your specific problem.
             
             Find out more in [iris-auto.com](https://iris-auto.com/)
             """
 
 modules = {"defect_detection": "Defect Detection",
            "object_detection": "Object Detection",
-           "ocr": "Optical Character Recognition",
+           "ocr_tool": "Optical Character Recognition",
            "measurement_tool": "Measurement Tool"
            }
 
@@ -53,11 +57,33 @@ obj_det_instructions = {
     "datasets": ["cars", "pills", "suitcase", "toothbrush"],
 }
 
+mes_instructions = {
+    "train": """
+            ### Train the model
+            """,
+    "evaluate": """
+                ### Evaluate the model
+                The results will appear here.
+                """,
+    "datasets": ["measurement"],
+}
+
+ocr_instructions = {
+    "train": """
+            ### Train the model
+            """,
+    "evaluate": """
+                ### Evaluate the model
+                The results will appear here.
+                """,
+    "datasets": ["ocr"],
+}
+
 instructions = {
     "defect_detection": def_det_instructions,
     "object_detection": obj_det_instructions,
-    "ocr": {},
-    "measurement_tool": {},
+    "ocr_tool": ocr_instructions,
+    "measurement_tool": mes_instructions,
 }
 
 primary_color = "blue"
